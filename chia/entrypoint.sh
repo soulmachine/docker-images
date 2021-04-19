285393ee847556b01da3c1154840d8349c5f70d8
soulmachine <soulmachine@gmail.com>
2021-04-19 04:07:33 -0700

    Recorded running time

--- chia/entrypoint.sh
+++ chia/entrypoint.sh
@@ -4,4 +4,11 @@ cd /chia-blockchain
 
 chia init
 
+start_time="$(date -u +%s)"
+
 exec "$@"
+
+end_time="$(date -u +%s)"
+
+elapsed="$(($end_time-$start_time))"
+echo "Time elapsed $elapsed seconds"
