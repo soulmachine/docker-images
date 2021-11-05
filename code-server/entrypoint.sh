cbe27bdd70efc553cc8c8f79e7102ab1a32159d0
soulmachine <soulmachine@gmail.com>
2021-11-05 11:24:49 +0000

    tiny bugfix

--- code-server/entrypoint.sh
+++ code-server/entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 set -eu
 
-echo "Starting coder-server, please wait(chown inside docker is slow)"
+echo "Starting container, please wait...(chown inside docker is slow)"
 
 eval "$(fixuid -q)"
 
-echo "The default password is passw0rd, you can change the password by setting the \$PASSWORD environment variable"
-
-/usr/bin/code-server "$@"
+case "$1" in
+    "code-server")
+        shift
+        echo "The default password is passw0rd, you can change the password by setting the \$PASSWORD environment variable"
+        /usr/bin/code-server "$@"
+    ;;
+    *)
+        exec "$@"
+    ;;
+esac
