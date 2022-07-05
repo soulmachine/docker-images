0abc2049b6b5830400a9564b912b4905ed5de7c2
soulmachine <soulmachine@gmail.com>
2022-07-05 09:53:47 +0000

    Upgraded to llvm 14 and code-server 4.5.0

--- code-server/Dockerfile.cpp
+++ code-server/Dockerfile.cpp
@@ -2,6 +2,6 @@ FROM soulmachine/code-server:cpp-base
 
 COPY --chown=$USER:$USER ./.vscode/settings-cpp.json /data/.local/share/code-server/User/settings.json
 
-RUN wget --waitretry 15 --tries 5 --no-verbose https://github.com/microsoft/vscode-cpptools/releases/download/1.7.1/cpptools-linux.vsix \
+RUN wget --waitretry 15 --tries 5 --no-verbose https://github.com/microsoft/vscode-cpptools/releases/download/v1.10.7/cpptools-linux.vsix \
  && code-server --install-extension ./cpptools-linux.vsix \
  && rm ./cpptools-linux.vsix
